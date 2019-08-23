a2f41048238989e452657f8ebf1c2bf071992744
Wilber Medina <wilbermedina@outlook.com>
2019-08-22 22:19:28 -0500

    nueva sesion

---
+++ Lab_Programacion/Semana 2/donet-session 1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace wmedina___sesion_1 /*paquete */
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My app: ");
+            Console.WriteLine("Ingrese su nobre:");
+            String name = Console.ReadLine();
+            Console.WriteLine("Ingrese su pasword: ");
+            String pass = Console.ReadLine();
+            Console.WriteLine("Ingrese sus edad: ");
+            int edad = int.Parse(Console.ReadLine());
+            Console.WriteLine("Hello {0}", name);
+            if (edad > 18)
+            {
+                Console.WriteLine("Puede Beber!");
+            }
+            else
+            {
+                Console.WriteLine("No debe Beber!");
+            }
+
+        }
+    }
+}
