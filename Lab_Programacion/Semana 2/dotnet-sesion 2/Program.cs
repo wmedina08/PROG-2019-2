a39f047baf45cda6733768884b29057c7f098cf7
Wilber Medina <wilbermedina@outlook.com>
2019-08-22 22:36:53 -0500

    change

--- Lab_Programacion/Semana 2/dotnet-sesion 2/Program.cs
+++ Lab_Programacion/Semana 2/dotnet-sesion 2/Program.cs
@@ -8,17 +8,17 @@ namespace dotnet_sesion_2
         static void Main(string[] args)
         {
             Console.WriteLine("Ingrese su nombre: ");
-            String nombre=Console.ReadLine();
+            String nombre = Console.ReadLine();
             Console.WriteLine("Ingrese su origen: ");
-            String origen=Console.ReadLine();
+            String origen = Console.ReadLine();
             Console.WriteLine("Ingrese su destino: ");
-            String destino=Console.ReadLine();
+            String destino = Console.ReadLine();
             Console.WriteLine("Ingrese su distancia: ");
-            double distancia=Double.Parse(Console.ReadLine());
+            double distancia = Double.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese su velocidad: ");
-            double velocidad=Double.Parse(Console.ReadLine());              
-            double tiempo=distancia/velocidad;
-            Console.WriteLine("Estimado {0}, usted llegara de {1}, a {2},en {3}, segundos, gracias",nombre, origen,destino,tiempo);
+            double velocidad = Double.Parse(Console.ReadLine());
+            double tiempo = distancia / velocidad;
+            Console.WriteLine("Estimado {0}, usted llegara de {1}, a {2},en {3}, segundos, gracias", nombre, origen, destino, tiempo);
         }
     }
 }
