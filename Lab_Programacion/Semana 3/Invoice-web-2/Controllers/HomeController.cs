b95beaecd1d26e972db691910bd3a523d0484d47
Wilber Medina <wilbermedina@outlook.com>
2019-08-29 17:49:02 -0500

    HomeController

--- Lab_Programacion/Semana 3/Invoice-web-2/Controllers/HomeController.cs
+++ Lab_Programacion/Semana 3/Invoice-web-2/Controllers/HomeController.cs
@@ -5,31 +5,32 @@ using Invoice_web_2.Models;
 namespace Invoice_web_2.Controllers
 {
 
-    public class HomeController:Controller{
+    public class HomeController : Controller
+    {
 
 
-        public IActionResult Index(){
+        public IActionResult Index()
+        {
             Console.WriteLine("Index");
             return View();
         }
-        
+
         // POST: Home/RegistrarFan
         [HttpPost]
-        public IActionResult calculo (Invoice invoice){
+        public IActionResult calculo(Invoice invoice)
+        {
             Console.WriteLine("calculo");
-            
-            Random random= new Random();
+
+            Random random = new Random();
             int num = random.Next();
-            invoice.Num=num;
+            invoice.Num = num;
 
-            int prod=invoice.producto;
-            int cant=invoice.cantidad;
-            int subt=prod*cant;
-            invoice.SubTotal=subt;
+            int subt = invoice.producto * invoice.cantidad; ;
+            invoice.SubTotal = subt;
 
-            double total=subt*1.18;
-            invoice.Total=total;
-            
+            double total = subt * 1.18;
+            invoice.Total = total;
+
             return View(invoice);
         }
     }
