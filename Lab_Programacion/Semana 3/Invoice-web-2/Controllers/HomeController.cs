e24270c4338eebe91ecf176e1a08b5aea44eb15e
Wilber Medina <wilbermedina@outlook.com>
2019-08-30 13:02:58 -0500

    cambio

--- Lab_Programacion/Semana 3/Invoice-web-2/Controllers/HomeController.cs
+++ Lab_Programacion/Semana 3/Invoice-web-2/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace Invoice_web_2.Controllers
             return View();
         }
 
-        // POST: Home/RegistrarFan
+       
         [HttpPost]
         public IActionResult calculo(Invoice invoice)
         {
@@ -25,7 +25,7 @@ namespace Invoice_web_2.Controllers
             int num = random.Next();
             invoice.Num = num;
 
-            int subt = invoice.producto * invoice.cantidad; ;
+            int subt = invoice.producto * invoice.cantidad; 
             invoice.SubTotal = subt;
 
             double total = subt * 1.18;
