0ba4098b9b7b7343d36c86bbef082985404a6288
Wilber Medina <wilbermedina@outlook.com>
2019-09-03 18:54:20 -0500

    semana 5

---
+++ Teoria_Programacion/Semana 5/a/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Semana_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ENTRADA
+            Console.WriteLine("¿Como te llamas?");
+            var nombre = Console.ReadLine();
+            Console.WriteLine("¿Que edad tienes?");
+            var edad=Convert.ToInt32(Console.ReadLine());
+            var edadAmigo=edad+5;
+            //SALIDAS
+            Console.WriteLine("Hello "+nombre);
+            Console.WriteLine("Hallo {0} " ,nombre);
+            Console.WriteLine($"Hola {nombre}");
+            Console.WriteLine(edad);
+            Console.WriteLine(edadAmigo);
+            Console.Write("Hola nena");
+            Console.Write("¡Como estas?");
+        }
+    }
+}
---
+++ Teoria_Programacion/Semana 5/b/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace b
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {/* 
+           var lista=new List<int>{15,20,8,13,12,15,15,20,0,6,12};
+           int apro;
+            int j=0;
+        for(int i=0; i<lista.Count;i++){
+            var num=lista[i];
+            if(num>10){
+                j++;
+            }
+        }
+        var aprobados =lista.Where(x => x>10);
+        var promedio =lista.Average();
+        */
+        var lista =new List<String>{"Manzana","Uva","Piña","Naranaja","Pera","Platano","Granadilla","Melon","Papaya"};
+        /* var fruta=lista.Where(x =>x.StartsWith("P"));
+            foreach(var item in fruta){
+            Console.WriteLine(item);
+        }
+        }
+        */
+        var fruta=lista.Where(x =>x.StartsWith("P"));
+        var up=fruta.First();
+        var ul=fruta.Last();
+        Console.WriteLine(up);
+        Console.WriteLine(ul);
+        }
+    }
+}
